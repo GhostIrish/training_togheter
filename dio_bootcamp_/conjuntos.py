22458e2a2ae5edfdbde3438836395244e5c81840
Hariel <harielpimenta@hotmail.com>
2024-09-02 18:32:03 -0300

    Atualizando estudos sobre conjuntos

--- dio_bootcamp_/conjuntos.py
+++ dio_bootcamp_/conjuntos.py
@@ -34,3 +34,37 @@ conjunto_c = {1, 2, 3}
 conjunto_d = {3, 4, 2}
 
 print(conjunto_c.intersection(conjunto_d))
+
+
+# este é um exemplo de diferença, que no caso só retorna oque há de diferente em 'e' que não tem em 'f'.
+conjunto_e = {1, 2, 3}
+conjunto_f = {3, 4, 2}
+
+print(conjunto_e.difference(conjunto_f))
+
+# aqui retorna a diferença simétrica, ou seja, oque há de diferente em ambos os conjuntos.
+conjunto_e = {1, 2, 3}
+conjunto_f = {3, 4, 2}
+
+print(conjunto_e.symmetric_difference(conjunto_f))
+
+
+# Esse verifica se todos os elementos do conjunto 'a' estão presentes no conjunto 'b', caso estejam
+# retorna True.
+conjunto_a = {1, 2, 3}
+conjunti_b = {4, 1, 2, 5, 6, 3}
+
+print(conjunto_a.issubset(conjunti_b))
+print(conjunto_b.issubset(conjunto_a))
+# o método .issuperset é ao contrário, se os elementos de 'a' estivrem todos em 'b', retorna falso.
+
+# o método .isdisjoint verifica se não ha nenhuma correlação entre os conjuntos, caso não tenha, 
+# retornará True.
+
+# o .add + valor desejado, adiciona um valor desejado ao conjunto
+
+# o .clear() limpa os conjuntos e o .copy() faz uma copia
+
+# o .discard(valor) descarta o valor desejado caso ele tenha no set
+
+# o .pop() vai tirando os primeiros valores do conjunto cada vez que é chamado.
