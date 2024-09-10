7364262dcfa7b1754024e8e1524deb2c2baf4982
Hariel <harielpimenta@hotmail.com>
2024-09-10 17:09:15 -0300

    Estudando módulos

---
+++ dio_bootcamp_/usando_datetime.py
@@ -0,0 +1,8 @@
+# Objetivo geral, trabalhar com datas, horas e fusos horários em python utilizando datetime
+
+from datetime import datetime, date
+
+d = date(2024, 9, 8)
+print(d)
+print()
+print(datetime.now())
