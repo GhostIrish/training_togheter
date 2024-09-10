e4dff22f9568a0db0c532a3975a04595f92447b4
Hariel <harielpimenta@hotmail.com>
2024-09-10 18:07:52 -0300

    Estudando strptime

--- dio_bootcamp_/usando_datetime.py
+++ dio_bootcamp_/usando_datetime.py
@@ -1,8 +1,26 @@
 # Objetivo geral, trabalhar com datas, horas e fusos horários em python utilizando datetime
 
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
 d = date(2024, 9, 8)
 print(d)
 print()
 print(datetime.now())
+print()
+print(datetime.today())
+
+d = d + timedelta(weeks=1)
+print(d) # adicionou uma semana a data de cima, funciona com days / minutes alem de weeks
+
+# usando strftime e strptime
+d2 = datetime.now()
+
+# formatando data e hora
+print(d2.strftime("%d/%m/%Y %H:%M"))
+
+# convertendo string para datetime
+date_string = '20/07/2023 15:30'
+d3 = datetime.strptime(date_string, "%d/%m/%Y %H:%M")
+print(d3)
+# também é possivel pegar uma data ou um datetime.now e converter 
+# ele para algum formato que vocÇe desejar
