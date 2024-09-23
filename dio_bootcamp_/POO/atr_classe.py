5bf4d292d8439832a2f088d69001a193b7600f19
Hariel <harielpimenta@hotmail.com>
2024-09-23 15:22:22 -0300

    Aprendendo sobre variaveis de classe e variaveis de instancia

---
+++ dio_bootcamp_/POO/atr_classe.py
@@ -0,0 +1,25 @@
+# atributos do objeto
+
+# ex:
+class Estudante:
+  escola = 'Dio'
+
+  def __init__(self, nome, numero):
+    self.nome = nome
+    self.numero = numero
+
+  def __str__(self):
+    return f'{self.nome}  ({self.numero} - {self.escola})'
+
+gui = Estudante('Gui', 165413)
+gi = Estudante('Gi', 165789)
+
+print(gui)
+print()
+print(gi)
+
+# variaveis de instancia são variaveis unicas por objeto, exemplo, se eu trocar o numero da matricula de um estudante
+# vai trocar somente o numero da matricula dele.
+# mas se eu trocar a variável da classe, que no caso do ex é o 'Dio', ele vai trocar para todos os outros 
+# objetos também.
+
